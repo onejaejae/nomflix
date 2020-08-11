fa531aaa920e5d2a38fc445110a56f725ac0153c
Cho Won Jae <yoteamo7@naver.com>
2020-08-11 13:47:37 +0900

    Updates

---
+++ precache-manifest.cc51ff7f864bcb0ca7742d0b100254f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7317744ee93847ff58ddf9ce2d4f1710",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "88421f78d40449d3b735",
+    "url": "/nomflix/static/js/2.cc036742.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/nomflix/static/js/2.cc036742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb56a3ae421d50fdf69",
+    "url": "/nomflix/static/js/main.38128429.chunk.js"
+  },
+  {
+    "revision": "7f0b2571cc398dde6626",
+    "url": "/nomflix/static/js/runtime-main.181cbd9a.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/nomflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
